219e44adecf63e437a400f08e7e81f85663aceb6
Azusfin <Azusfin@users.noreply.github.com>
2022-01-12 19:50:02 +0700

    feat: add img assets

---
+++ dist/utils/img/XO.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.OImg = exports.XImg = void 0;
+const canvas_1 = require("canvas");
+exports.XImg = (0, canvas_1.loadImage)("assets/X.png");
+exports.OImg = (0, canvas_1.loadImage)("assets/O.png");
---
+++ src/utils/img/XO.ts
@@ -0,0 +1,4 @@
+import { loadImage } from "canvas";
+
+export const XImg = loadImage("assets/X.png")
+export const OImg = loadImage("assets/O.png")
